20f9f220f995094ce5d0824fffa7d66fbc47b5f7
Pico <46362299+picoxr@users.noreply.github.com>
2021-09-24 20:26:41 +0800

    MiracastInApp-Demo

---
+++ Assets/Scripts/MiracastInApp.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.PXR;
+using UnityEngine;
+using UnityEngine.UI;
+using LitJson;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+
+public class MiracastInApp : MonoBehaviour
+{
+    public Text reminder;
+    public Text[] deviceName;
+
+
+    private bool selectDevice = false;
+    private bool isMiracastOn = false;
+    private string modelJson;
+    public List<string> content = new List<string>();
+
+    public void OpenMiracast()
+    {
+        PXR_System.OpenMiracast();
+        isMiracastOn = PXR_System.IsMiracastOn();
+        if (isMiracastOn)
+        {
+            reminder.color = Color.red;
+            reminder.text = "Open success";
+        }
+        else
+        {
+            reminder.text = "Open failed";
+        }
+    }
+    public void StartScan()
+    {
+
+
+
+        if (isMiracastOn)
+        {
+            PXR_System.StartScan();
+
+            Debug.Log("Mircast ON!");
+            PXR_System.SetWDJsonCallback();
+            PXR_System.UpdateWifiDisplays(parsejsonfile);
+            reminder.text = "Select the device";
+        }
+        else
+        {
+            Debug.Log("Mircast OFF!");
+            reminder.text = " Please turn on the screencast function";
+        }
+    }
+
+
+
+    public void StartProjection()
+    {
+        if (selectDevice && isMiracastOn)
+        {
+            PXR_System.ConnectWifiDisplay(modelJson);
+
+        }
+        else
+        {
+            reminder.text = "Please turn on the screencast function and select the device first";
+        }
+    }
+
+    public void StopProjection()
+    {
+        PXR_System.DisConnectWifiDisplay();
+    }
+
+
+    public void parsejsonfile(string obj)
+    {
+
+        JArray jArray = JArray.Parse(obj);
+        for (int i = 0; i < jArray.Count; i++)
+        {
+            Debug.Log("obj:" + obj);
+            JsonData data = new JsonData();
+            JObject job = (JObject)jArray[i];
+            string StrdeviceAddress = job["b"].ToString();
+            string StrdeviceName = job["c"].ToString();
+            string StrisAvailable = job["d"].ToString();
+            string StrcanConnect = job["e"].ToString();
+            string StrisRemembered = job["f"].ToString();
+            string StrstatusCodeg = job["g"].ToString();
+            Debug.Log("g :" + StrstatusCodeg);
+            if (i < 5)
+            {
+                deviceName[i].text = StrdeviceName;
+                data["deviceAddress"] = StrdeviceAddress;
+                data["deviceName"] = StrdeviceName;
+                data["isAvailable"] = StrisAvailable;
+                data["canConnect"] = StrcanConnect;
+                data["isRemembered"] = StrisRemembered;
+                data["statusCode"] = StrstatusCodeg;
+                data["status"] = "";
+                data["description"] = "";
+                string json = data.ToJson();
+                content[i] = json;
+                Debug.Log("json:" + json);
+                Debug.Log("content:" + content[i]);
+            }
+
+        }
+
+    }
+
+    public void deviceBtn1()
+    {
+        selectDevice = true;
+        modelJson = content[0];
+        reminder.text = deviceName[0].text;
+
+    }
+    public void deviceBtn2()
+    {
+        selectDevice = true;
+        modelJson = content[1];
+        reminder.text = deviceName[1].text;
+
+    }
+    public void deviceBtn3()
+    {
+        selectDevice = true;
+        modelJson = content[2];
+        reminder.text = deviceName[2].text;
+
+    }
+    public void deviceBtn4()
+    {
+        selectDevice = true;
+        modelJson = content[3];
+        reminder.text = deviceName[3].text;
+
+    }
+    public void deviceBtn5()
+    {
+        selectDevice = true;
+        modelJson = content[4];
+        reminder.text = deviceName[4].text;
+
+    }
+
+    public void closeMiracast()
+    {
+        PXR_System.CloseMiracast();
+        isMiracastOn = PXR_System.IsMiracastOn();
+        if (isMiracastOn == false)
+        {
+            reminder.text = "Close success";
+        }
+        else
+        {
+            reminder.text = "Close failed";
+        }
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/emm.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.PXR;
+using UnityEngine;
+
+public class emm : MonoBehaviour
+{
+    //初始化并绑定Service，其中objectName为接收回调的脚本物体名称
+    private void Awake()
+    {
+        PXR_System.InitSystemService(this.name);
+        PXR_System.BindSystemService();
+
+    }
+    //解除绑定Service
+    private void OnDestory()
+    {
+        PXR_System.UnBindSystemService();
+    }
+    //增加4个接收回调方法
+    private void BoolCallback(string value)
+    {
+        if (PXR_Plugin.System.BoolCallback != null) PXR_Plugin.System.BoolCallback(bool.Parse(value));
+        PXR_Plugin.System.BoolCallback = null;
+    }
+    private void IntCallback(string value)
+    {
+        if (PXR_Plugin.System.IntCallback != null) PXR_Plugin.System.IntCallback(int.Parse(value));
+        PXR_Plugin.System.IntCallback = null;
+    }
+    private void LongCallback(string value)
+    {
+        if (PXR_Plugin.System.LongCallback != null) PXR_Plugin.System.LongCallback(int.Parse(value));
+        PXR_Plugin.System.LongCallback = null;
+    }
+    private void StringCallback(string value)
+    {
+        if (PXR_Plugin.System.StringCallback != null) PXR_Plugin.System.StringCallback(value);
+        PXR_Plugin.System.StringCallback = null;
+    }
+    public void toBServiceBind(string s) { Debug.Log("Bind success."); }
+}
